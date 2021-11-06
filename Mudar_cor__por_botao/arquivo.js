12e7ce5cc5db2d1fb9543e5502761aa6f293e633
GlauberRoque <glauberroqueferr@gmail.com>
2021-11-05 21:30:33 -0300

    exercicio1

---
+++ Mudar_cor__por_botao/arquivo.js
@@ -0,0 +1,23 @@
+function vermelho(vermelho) {
+
+    document.body.style.background = 'red';
+
+}
+
+function azul(azul) {
+
+    document.body.style.background = 'blue';
+
+}
+
+function verde(verde) {
+
+    document.body.style.background = 'green';
+
+}
+
+function amarelo(amarelo) {
+
+    document.body.style.background = 'yellow';
+
+}
