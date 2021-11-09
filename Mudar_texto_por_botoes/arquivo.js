c25cddc88deba69a3dbbf777672644359cc0f922
GlauberRoque <glauberroqueferr@gmail.com>
2021-11-09 18:19:22 -0300

    exercicio6 Atualizado

--- Mudar_texto_por_botoes/arquivo.js
+++ Mudar_texto_por_botoes/arquivo.js
@@ -5,3 +5,27 @@ function fonte(cor) {
 function fundo(cor) {
     document.getElementById("texto").style.backgroundColor = cor;
 }
+
+var tamanho = 16;
+
+function aumentarFonte() {
+    tamanho++;
+    document.getElementById("texto").style.fontSize = tamanho + "px";
+}
+
+function diminuirFonte() {
+    tamanho--;
+    document.getElementById("texto").style.fontSize = tamanho + "px";
+}
+
+function maiusculo() {
+    var maiuscula = document.getElementById("texto").innerHTML;
+    maiuscula = maiuscula.toUpperCase();
+    document.getElementById("texto").innerHTML = maiuscula;
+}
+
+function minuscula() {
+    var minuscula = document.getElementById("texto").innerHTML;
+    minuscula = minuscula.toLowerCase();
+    document.getElementById("texto").innerHTML = minuscula;
+}
